25358edf82da98309de559ac97365ad0266974cd
fikrinazilul1690 <fikrinazilul@gmail.com>
2023-10-09 00:15:46 +0700

    08/10/2023

--- src/app/(customer)/payment/[paymentId]/page.tsx
+++ src/app/(customer)/payment/[paymentId]/page.tsx
@@ -5,8 +5,9 @@ import { Divider } from '@nextui-org/divider';
 import { getServerSession } from 'next-auth';
 import { notFound, redirect } from 'next/navigation';
 import React from 'react';
-import Checkout from '../../order/components/Checkout';
 import Image from 'next/image';
+import { formatIDR } from '@/lib/formatIDR';
+import { Button } from '@nextui-org/button';
 
 export default async function DetailPayment({
   params,
@@ -24,48 +25,59 @@ export default async function DetailPayment({
   );
   if (!!!detailPayment) notFound();
   return (
-    <main className='border py-3 px-7 max-w-3xl mx-auto mt-5 rounded-lg'>
-      <h3 className='text-2xl font-semibold text-center'>Detail Pembayaran</h3>
-      <section className='my-4'>
-        <div className='flex justify-between'>
-          <span className='text-lg font-bold'>Metode Pembayaran</span>
-          <Image
-            alt={detailPayment.bank}
-            src={
-              listBank.find((val) => val.code === detailPayment.bank)
-                ?.imageUrl ?? ''
-            }
-            width={60}
-          />
-        </div>
-        <Divider className='my-2' />
-      </section>
-      <section className='my-4'>
-        <div className='flex justify-between'>
-          <div className='flex flex-col gap-2'>
-            <span className='text-lg'>Nomor Virtual Account</span>
-            <span className='font-bold text-lg'>{detailPayment.va_number}</span>
+    <main className='border max-w-3xl mx-auto rounded-lg mt-5'>
+      <div className='py-10 px-7'>
+        <h3 className='text-2xl font-semibold text-center'>
+          Detail Pembayaran
+        </h3>
+        <section className='my-4'>
+          <div className='flex justify-between'>
+            <span className='text-lg font-bold'>Metode Pembayaran</span>
+            <Image
+              alt={detailPayment.bank}
+              src={
+                listBank.find((val) => val.code === detailPayment.bank)
+                  ?.imageUrl ?? ''
+              }
+              width={120}
+              height={100}
+            />
           </div>
-          <Image
-            alt={detailPayment.bank}
-            src={
-              listBank.find((val) => val.code === detailPayment.bank)
-                ?.imageUrl ?? ''
-            }
-            width={60}
-          />
-        </div>
-        <Divider className='my-2' />
-      </section>
-      <section className='my-4'>
-        <h4 className='text-xl font-semibold'>Pengiriman</h4>
-        <Divider className='my-2' />
-      </section>
-      <section className='my-4'>
-        <h4 className='text-xl font-semibold'>Total Belanja</h4>
-        <Divider className='my-2' />
-        <Checkout />
-      </section>
+          <Divider className='my-2' />
+        </section>
+        <section className='my-4'>
+          <div className='flex justify-between'>
+            <div className='flex flex-col gap-2'>
+              <span className='text-lg'>Nomor Virtual Account</span>
+              <span className='font-bold text-lg'>
+                {detailPayment.va_number}
+              </span>
+            </div>
+          </div>
+          <Divider className='my-2' />
+        </section>
+        <section className='my-4'>
+          <div className='flex justify-between'>
+            <div className='flex flex-col gap-2'>
+              <span className='text-lg'>Total Pembayaran</span>
+              <span className='font-bold text-lg'>
+                {formatIDR(detailPayment.gross_amount)}
+              </span>
+            </div>
+          </div>
+          <Divider className='my-2' />
+        </section>
+        <section className='my-4'>
+          <div className='flex gap-5 items-center'>
+            <Button variant='bordered' fullWidth color='primary'>
+              Cek Status Pembayaran
+            </Button>
+            <Button variant='solid' fullWidth color='primary'>
+              Belanja Lagi
+            </Button>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
